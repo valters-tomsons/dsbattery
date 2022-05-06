33329b3c26bb50e8b94db55418e127e14aab4856
Valters <valters.tomsons@protonmail.com>
2022-05-06 17:31:56 +0300

    Update release script

--- build_release.sh
+++ build_release.sh
@@ -2,4 +2,6 @@
 ARCH="linux-x64"
 
 dotnet publish -c Release -r $ARCH --self-contained -o release_build
+cp modules release_build/ -r
+cp README.md release_build/
 echo "dsbattery release created for $ARCH"
